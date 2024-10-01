6a21417a32a5223eec387d50850bd9ace3554f51
MohamedAdelEid <01114979112mohamed@gmail.com>
2024-10-01 15:29:38 +0300

    🔧 refactor(create , edit property) remove required form price_after_discount , installment

--- resources/views/admin/property/create.blade.php
+++ resources/views/admin/property/create.blade.php
@@ -127,7 +127,7 @@
                         <input type="number" name="price_after_discount" id="discount-amount"
                             value="{{ old('price_after_discount') }}"
                             class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 transition duration-200"
-                            placeholder="ادخل السعر بعد الخصم " required />
+                            placeholder="ادخل السعر بعد الخصم " />
                         @error('price_after_discount')
                             <p class="text-red-500 text-xs italic">{{ $message }}</p>
                         @enderror
@@ -141,7 +141,7 @@
                         <input type="number" name="installment_amount" id="installment-amount"
                             value="{{ old('installment_amount') }}"
                             class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 transition duration-200"
-                            placeholder="ادخل المقدم " required>
+                            placeholder="ادخل المقدم " />
                         @error('installment_amount')
                             <p class="text-red-500 text-xs italic">{{ $message }}</p>
                         @enderror
--- resources/views/admin/property/edit.blade.php
+++ resources/views/admin/property/edit.blade.php
@@ -125,7 +125,7 @@
                         <input type="number" name="price_after_discount" id="discount-amount"
                             value="{{ old('price_after_discount', $property->price_after_discount) }}"
                             class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 transition duration-200"
-                            placeholder="ادخل السعر بعد الخصم " required />
+                            placeholder="ادخل السعر بعد الخصم " />
                     </div>
                 </div>
 
@@ -136,7 +136,7 @@
                         <input type="number" name="installment_amount" id="installment-amount"
                             value="{{ old('installment_amount', $property->installment_amount) }}"
                             class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 transition duration-200"
-                            placeholder="ادخل المقدم " required>
+                            placeholder="ادخل المقدم " />
                     </div>
                 </div>
 
