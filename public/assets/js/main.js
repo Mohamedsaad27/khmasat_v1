65b2833d2a6fd4418ab396125142977721747f5e
MohamedAdelEid <01114979112mohamed@gmail.com>
2024-10-06 04:46:17 +0300

    refactor( preloader ) using document

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,16 +1,16 @@
 // Remove Preloader When load page
-window.addEventListener("load", function () {
+document.addEventListener("DOMContentLoaded", function () {
+
     const preloader = document.getElementById("preloader");
 
-    // Fade out the preloader
     preloader.style.opacity = 0;
 
-    // Remove z-index after the fade out
-    setTimeout(() => {
+    setTimeout(function () {
         preloader.classList.remove('z-[50000]');
-        preloader.style.display = "none"; // Optionally remove from view
-    }, 500); // Match the duration of your fade-out animation (500ms)
-})
+        preloader.style.display = "none";
+    }, 200);
+});
+
 
 /* =============== ♥_♥ =============== */
 
