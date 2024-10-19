cc0aff876b417c443945359fb5ab9b12dfa0c594
MohamedAdelEid <01114979112mohamed@gmail.com>
2024-10-19 15:27:42 +0300

    🔧refactor files ( slidebarAdmin in config , web ) after conflict

--- config/admin/sidebarItems.php
+++ config/admin/sidebarItems.php
@@ -72,5 +72,27 @@ return [
                 'active' => 'admin/properties'
             ]
         ]
+    ],
+    [
+        'icon' => '<svg class="flex-shrink-0 w-6 h-6 text-inherit transition duration-200 group-hover:text-gray-900 dark:group-hover:text-white"
+                        fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path
+                        d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z">
+                        </path>
+                    </svg>',
+        'title' => 'مميزات العقارات',
+        'active' => 'admin/benefit-properties*',
+        'childItems' => [
+            [
+                'title' => 'اضافة ميزة جديد',
+                'route' => 'properties.create',
+                'active' => 'admin/benefit-properties/create'
+            ],
+            [
+                'title' => 'عرض المميزات',
+                'route' => 'properties.index',
+                'active' => 'admin/benefit-properties'
+            ]
+        ]
     ]
 ];
