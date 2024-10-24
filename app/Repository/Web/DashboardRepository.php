2b5dc01613672d6d20e9dd8b2a0f4af85ae601e2
MohamedAdelEid <01114979112mohamed@gmail.com>
2024-10-24 22:14:26 +0300

    - refactor( confilct dashboard repository )

--- app/Repository/Web/DashboardRepository.php
+++ app/Repository/Web/DashboardRepository.php
@@ -1,7 +1,13 @@
 <?php
 
-namespace App\Repository;
+namespace App\Repository\Web;
 use App\Interfaces\DashboardRepositoryInterface;
 
 class DashboardRepository implements DashboardRepositoryInterface
 {
+
+    public function index()
+    {
+        return view('admin.index');
+    }
+}
